a034b340a2086a7c2931c8cd66acb502362d7e6d
cljforge <motorniy.freshcode@gmail.com>
2016-02-15 13:05:01 +0200

    columns

--- scroll/scroll.js
+++ scroll/scroll.js
@@ -1,13 +1,44 @@
 var wheelAngle = 0;
+var backOffset = 0;
+var currentAnchor;
 
-function scrollTo(el){
+var screens = 
+[{
+	anchor: "first",
+	content: "<b>first</b>"
+},
+{
+	anchor: "second",
+	content: "<strike>second</strike>"
+},
+{
+	anchor: "third",
+	content: "<u>third</u>"
+}];
+
+function scrollToAnchor(el){
+	currentAnchor = el;
 	var time = 0.3;
 
-	var caretOffset = $('.anchor-'+el).position().top - sBlockHeight/2 + $('.anchor').outerHeight()/2
+	var caretOffset = $('.anchor-'+el).position().top - sBlockHeight/2 + $('[class*=anchor-]').outerHeight()/2;
 
 	$('.container').animate({
 		scrollTop: $('#'+el).position().top
 	}, time*1000);
+	
+	var delta = $('.scroll-caret').position().top - caretOffset;
+	if(delta < -1 || delta > 1){
+		if(delta > 0){
+			backOffset +=10;
+		} else {
+			backOffset -=10;
+		}
+	}
+
+	$('body').css({
+		'transition' : 'background-position ' + time + 's linear',
+		'background-position' : '0px ' + backOffset + "%"
+	})
 
 	$('.scroll-caret').css({
 		'transition' : 'top ' + time + 's linear',
@@ -23,7 +54,7 @@ function scrollTo(el){
 		'background-position': '0px ' + caretOffset + "px"
 	});
 
-	wheelAngle = wheelAngle + caretOffset*1000;
+	wheelAngle = wheelAngle + delta;
 
 	$('.wheel-spinner').css({
 		'transition' : 'transform ' + time + "s linear",
@@ -31,7 +62,19 @@ function scrollTo(el){
 	})
 }
 
+function wheelerScroll(direction){
+	var destination = direction + screens.findIndex(function(el){
+		return el.anchor == currentAnchor;
+	});
+
+	if(destination != -1 && destination != screens.length){
+		scrollToAnchor(screens[destination].anchor);
+	}
+}
+
 $(document).ready(function(){
+
+	$(window).on('resize',scale)
 
 	var $body = $('body'),
 	$sC = $('.scroll-caret'),
@@ -47,42 +90,31 @@ $(document).ready(function(){
 
 		for (var i = 0; i < screens.length; i++) {
 			$C.append('<div id="'+ screens[i].anchor +'" class="screen">' + screens[i].content + "</div>");
-			$sB.append('<div class="anchor anchor-'+ screens[i].anchor +'" onclick="scrollTo(\''+ screens[i].anchor +'\')">' + screens[i].anchor + "</div>")
+			$sB.append('<div class="anchor-'+ screens[i].anchor +'" onclick="scrollToAnchor(\''+ screens[i].anchor +'\')">' + screens[i].anchor + "</div>")
 		};
 
 		sBlockHeight = ($sB[0].clientHeight / screens.length);
 
 		$('.scroll-caret').css('height', sBlockHeight); 
 		$('.arrow').css('top' , $('.scroll-caret').outerHeight()/2 - $('.arrow').outerHeight()/2);
-		$('.anchor').each(function(index, el){
+		$('[class*=anchor-]').each(function(index, el){
 			$(el).css({
-				"top": (sBlockHeight * (index+1) - (sBlockHeight/2)) - $('.anchor').outerHeight()/2,
+				"top": (sBlockHeight * (index+1) - (sBlockHeight/2)) - $('[class*=anchor-]').outerHeight()/2,
 				"left": $sB.outerWidth() + 15
 			})
-		})	
+		})
+
+		wheelerScroll(0);
 	}
 
-	var screens = 
-	[{
-		anchor: "first",
-		content: "<b>first</b>"
-	},
-	{
-		anchor: "second",
-		content: "<strike>second</strike>"
-	},
-	{
-		anchor: "third",
-		content: "<u>third</u>"
-	}];
-
+	currentAnchor = screens[0].anchor;
 	scale();
 
-	$body.on('click', '#wheel-top .wheel-button', function(){
-		Scroll($sC.offset().top - $sC.parent().offset().top - 30)
+	$body.on('click', '#wheel-top', function(){
+		wheelerScroll(-1)
 	})
 
-	$body.on('click', '#wheel-bot .wheel-button', function(){
-		Scroll($sC.offset().top - $sC.parent().offset().top + 30)
+	$body.on('click', '#wheel-bot', function(){
+		wheelerScroll(1)
 	})
 })

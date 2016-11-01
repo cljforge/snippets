295b7473fb33ab18bc7afdf16893349a37e67cfe
cljforge <motorniy.freshcode@gmail.com>
2016-11-01 09:22:34 +0200

    scroll fix

--- scroll/scroll.js
+++ scroll/scroll.js
@@ -2,7 +2,7 @@ var screens =
     [
         {
             anchor: "Main",
-            content: "<u>main</u>"
+            content: '<a id="copy-a" href="#">Copy</a>'
         },
 
         {
@@ -21,9 +21,9 @@ $(document).ready(function () {
         backOffset: 50,
         columnOffset: 0,
         sBlockHeight: 0,
-        $C: $('.container'),
-        $SB: $('.scroll-bar'),
-        $SC: $('.scroll-caret'),
+        container: $('.container'),
+        scrollBar: $('.scroll-bar'),
+        scrollCaret: $('.scroll-caret'),
         currentIndex: 0,
         inAction: false,
         screens: [],
@@ -39,7 +39,7 @@ $(document).ready(function () {
                 anchorHandler.scrollToIndex($(this).attr('data-index'));
             });
 
-            $body.on('click', '#wheel-top', function () {
+            $body.on('click', '#wheel-top', function (e) {
                 anchorHandler.wheelerScroll(-1)
             });
             $body.on('click', '#wheel-bot', function () {
@@ -49,8 +49,8 @@ $(document).ready(function () {
             this.screens = $.extend(true, {}, screens);
 
             for (var i = 0; i < screens.length; i++) {
-                this.$C.append('<div id="' + screens[i].anchor + '" class="screen">' + screens[i].content + "</div>", {});
-                this.$SB.append('<div class="anchor-' + i + '" data-index="' + i + '">' + screens[i].anchor + "</div>", {})
+                this.container.append('<div id="' + screens[i].anchor + '" class="screen">' + screens[i].content + "</div>", {});
+                this.scrollBar.append('<div class="anchor-' + i + '" data-index="' + i + '">' + screens[i].anchor + "</div>", {})
             }
 
             this.scale();
@@ -70,10 +70,10 @@ $(document).ready(function () {
                 this.inAction = true;
                 var time = 0.3,
                     caretOffset = $('.anchor-' + index).position().top - this.sBlockHeight / 2 + $('[class*=anchor-]').outerHeight() / 2,
-                    delta = this.$SC.position().top - caretOffset;
+                    delta = this.scrollCaret.position().top - caretOffset;
 
-                this.$C.animate({
-                    scrollTop: $('#' + this.screens[index].anchor).position().top
+                this.container.animate({
+                    scrollTop: $('#' + this.screens[index].anchor)[0].offsetTop-50
                 }, time * 1000);
 
                 if (delta < -1 || delta > 1) {
@@ -88,17 +88,17 @@ $(document).ready(function () {
 
                 // Columns
                 this.columnOffset += delta;
-                this.$C.css({
+                this.container.css({
                     'transition': 'background-position ' + time + 's linear',
                     'background-position': 'left ' + this.columnOffset + ', right ' + this.columnOffset + ', left ' + this.columnOffset + ', right ' + this.columnOffset
                 });
 
                 //ScrollBar
-                $('.scroll-caret').css({
+                this.scrollCaret.css({
                     'transition': 'top ' + time + 's linear',
                     'top': caretOffset
                 });
-                $('.scroll-bar').css({
+                this.scrollBar.css({
                     'transition': 'background-position ' + time + 's linear',
                     'background-position': '0px ' + caretOffset + "px"
                 });
@@ -122,21 +122,21 @@ $(document).ready(function () {
             return this;
         },
         scale: function () {
-            anchorHandler.$SB.css('height', $('.wrapper')[0].clientHeight - $('.wheel').first().outerHeight() * 2);
+            anchorHandler.scrollBar.css('height', $('.wrapper')[0].clientHeight - $('.wheel').first().outerHeight() * 2);
 
-            var bH = anchorHandler.$SB[0].clientHeight,
+            var bH = anchorHandler.scrollBar[0].clientHeight,
                 $arrow = $('.arrow'),
                 $anchors = $('[class*=anchor-]');
 
             anchorHandler.sBlockHeight = (bH / screens.length);
 
-            anchorHandler.$SC.css('height', anchorHandler.sBlockHeight);
-            $arrow.css('top', anchorHandler.$SC.outerHeight() / 2 - $arrow.outerHeight() / 2);
+            anchorHandler.scrollCaret.css('height', anchorHandler.sBlockHeight);
+            $arrow.css('top', anchorHandler.scrollCaret.outerHeight() / 2 - $arrow.outerHeight() / 2);
 
             $anchors.each(function (index, el) {
                 $(el).css({
                     "top": (anchorHandler.sBlockHeight * (index + 1) - (anchorHandler.sBlockHeight / 2)) - $anchors.outerHeight() / 2,
-                    "left": anchorHandler.$SB.outerWidth() + 15
+                    "left": anchorHandler.scrollBar.outerWidth() + 15
                 })
             });
 
@@ -144,6 +144,16 @@ $(document).ready(function () {
             return this;
         }
     };
-
     anchorHandler.init(screens);
+
+    $('.anchor-0').on({
+    	'click': function(){
+    		document.execCommand('copy')
+		},
+		'copy': function(e){
+            console.log(e)
+			e.originalEvent.clipboardData.setData("text/plain", "IT WORKS!")
+    		e.preventDefault()
+		}
+    })
 });
